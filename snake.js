6c964f0cef57d56e9235a6fdd8828edb967349d5
App Academy Student <student@appacademy.io>
2013-12-02 13:49:35 -0500

    Finished

---
+++ snake.js
@@ -0,0 +1,108 @@
+(function(root) {
+  var Game = root.Game = (root.Game || {});
+
+  var Snake = Game.Snake = function Snake() {
+    Snake.BOARD_WIDTH = 50;
+    Snake.BOARD_HEIGHT = 50;
+    this.dir = 'N';
+    this.segments = [new Coord(Snake.BOARD_WIDTH / 2, Snake.BOARD_HEIGHT / 2)];
+  }
+
+  Game.Snake.DIRECTION = {
+    'N' : [0, -1],
+    'S' : [0, 1],
+    'E' : [1, 0],
+    'W' : [-1, 0]
+  }
+
+  Snake.prototype.move = function() {
+    for (var i = this.segments.length - 1; i > 0; i--) {
+      this.segments[i].follow(this.segments[i - 1]);
+    }
+    this.segments[0].plus(this.dir);
+  };
+
+  Snake.prototype.turn = function(dir) {
+    this.dir = dir;
+  };
+
+  var Board = Game.Board = function Board(snake) {
+    this.snake = snake;
+    this.apples = [];
+  }
+
+  Board.prototype.render = function(el) {
+    el.html("");
+    for ( var y = 0; y < Snake.BOARD_HEIGHT; y++) {
+      el.append("<div id=\"y" + y + "\" class=\"row\"></div>");
+      for ( var x = 0; x < Snake.BOARD_WIDTH; x++) {
+        var segment = _.find(this.snake.segments, function(coord) {
+          return coord.x == x && coord.y == y;
+        });
+        var apple = _.find(this.apples, function(coord) {
+          return coord.x == x && coord.y == y;
+        });
+        $('#y' + y).append('<div class=\"square\"></div>');
+        var cur = $("#y" + y + " div").last();
+        if (segment) {
+          cur.addClass("snake");
+        } else if (apple) {
+          cur.addClass("apple");
+        }
+      }
+    }
+  }
+
+  Board.prototype.addApple = function() {
+    coord = new Coord(Math.floor(Math.random() * Game.Snake.BOARD_WIDTH), Math.floor(Math.random() * Game.Snake.BOARD_HEIGHT));
+    this.apples.push(coord);
+  }
+
+  Board.prototype.checkApple = function () {
+    var head = this.snake.segments[0];
+    var apple = _.find(this.apples, function(apple) {
+      return apple.x == head.x && apple.y == head.y;
+    });
+    if (apple) {
+      this.apples.splice(this.apples.indexOf(apple), 1);
+      var coord = new Coord(-1, -1);
+      coord.follow(_.last(this.snake.segments));
+      this.snake.segments.push(coord);
+      console.log(this.snake.segments);
+    }
+  }
+
+  Board.prototype.checkCollision = function () {
+    var head = this.snake.segments[0];
+    segs = this.snake.segments.slice(1);
+
+    var hitSelf = _.find(segs, function(coord) {
+      return coord.x == head.x && coord.y == head.y;
+    });
+
+    var offBoard = head.x < 0 ||
+      head.x > Game.Snake.BOARD_WIDTH ||
+      head.y < 0 ||
+      head.y > Game.Snake.BOARD_HEIGHT;
+
+    return hitSelf || offBoard;
+  }
+
+  function Coord(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  Coord.prototype.plus = function(dir) {
+    var dir_arr = Snake.DIRECTION[dir];
+    this.x += dir_arr[0];
+    this.y += dir_arr[1];
+  }
+
+  Coord.prototype.follow = function(other) {
+    this.x = other.x;
+    this.y = other.y;
+  }
+
+
+})(this);
---
+++ snakeui.js
@@ -0,0 +1,50 @@
+(function(root) {
+  var Game = root.Game = (root.Game || {});
+
+  var View = Game.View = function View(el) {
+    this.el = el;
+    this.timer = null;
+  }
+
+  View.prototype.start = function () {
+    var self = this;
+
+    var snake = new Game.Snake();
+    var board = new Game.Board(snake);
+    $(document).keydown(function(event) {
+      var key = event.keyCode;
+      if (key === 40) {
+        snake.turn('S');
+      } else if (key === 38) {
+        snake.turn('N');
+      } else if (key === 39) {
+        snake.turn('E');
+      } else if (key === 37) {
+        snake.turn('W');
+      }
+    });
+
+
+    this.timer = setInterval(function() {
+      self.step(board);
+    }, 500)
+    board.addApple();
+  }
+
+  View.prototype.step = function(board) {
+    board.checkApple();
+    board.snake.move();
+    if (Math.random() < 0.05) {
+      board.addApple();
+    }
+    if (board.checkCollision()) {
+      console.log(board.snake.segments);
+      alert("You lose!");
+      clearInterval(this.timer);
+      this.timer = null;
+    } else {
+      board.render(this.el);
+    }
+  };
+
+})(this);
